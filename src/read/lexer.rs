33855eaa22f7564a666477716536f2ba0107b218
Jayden <179274165+JaydenHuang555@users.noreply.github.com>
2025-12-20 22:02:27 -0800

    removed uneeded function

--- src/read/lexer.rs
+++ src/read/lexer.rs
@@ -33,18 +33,6 @@ impl LexState {
     }
 }
 
-pub fn prec_chars(value: char) -> i32 {
-    match value {
-        '(' | ')' => -1,
-        _ => {
-            let precedence = prec(value);
-            match precedence {
-                _ => precedence as i32,
-            }
-        }
-    }
-}
-
 pub fn postfix(infix_equation: &str) -> Vec<Token> {
     let mut buffer: Vec<Token> = Vec::new();
     let mut operators: Vec<Token> = Vec::new();
