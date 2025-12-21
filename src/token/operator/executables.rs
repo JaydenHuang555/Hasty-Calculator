99317efa82357570d54f97c8458ec870fc610668
Jayden <179274165+JaydenHuang555@users.noreply.github.com>
2025-12-20 19:33:01 -0800

    executable constants

--- src/token/operator/executables.rs
+++ src/token/operator/executables.rs
@@ -1,6 +1,5 @@
 use crate::token::operator::executable::OperatorExecutable;
 
-
 pub const ADD: OperatorExecutable = OperatorExecutable::new("+", |left, right| -> f64 {left + right});
 pub const SUB: OperatorExecutable = OperatorExecutable::new("-", |left, right| -> f64 {left - right});
 
