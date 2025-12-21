62980765a75375822f8c68ca0215689861800c56
Jayden <179274165+JaydenHuang555@users.noreply.github.com>
2025-12-20 21:00:26 -0800

    parantheses

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,19 +1,22 @@
 use std::fmt::Display;
 
-use crate::token::operator::executable::OperatorExecutable;
+use crate::token::{operator::executable::OperatorExecutable, parantheses::ParanthesesType};
 
 pub mod operator;
+pub mod parantheses;
 
 pub enum Token {
     Num(f64),
-    Operator(OperatorExecutable)
+    Operator(OperatorExecutable),
+    Parentheses(ParanthesesType)
 }
 
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Num(operand) => write!(f, "{}", operand),
-            Self::Operator(executable) => write!(f, "{}", executable)
+            Self::Operator(executable) => write!(f, "{}", executable),
+            Self::Parentheses(parantheses_type) => parantheses_type.fmt(f)
         }
     }
 }
---
+++ src/token/parantheses.rs
@@ -0,0 +1,21 @@
+use std::fmt::Display;
+
+
+
+pub enum ParanthesesType {
+    Open,
+    Close,
+}
+
+impl Display for ParanthesesType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f, 
+            "{}", 
+            match self {
+                Self::Open => "(",
+                Self::Close => ")"
+            }
+        )
+    }
+}
