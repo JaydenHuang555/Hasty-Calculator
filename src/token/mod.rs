38dd5fe3e7f5e3cc5f9bf96bcf810c3f6fec7904
Jayden <179274165+JaydenHuang555@users.noreply.github.com>
2025-12-20 20:20:15 -0800

    token module

--- src/token/mod.rs
+++ src/token/mod.rs
@@ -1,8 +1,19 @@
+use std::fmt::Display;
+
 use crate::token::operator::executable::OperatorExecutable;
 
 pub mod operator;
 
-pub enum Token<'a> {
+pub enum Token {
     Num(f64),
-    Operator(OperatorExecutable<'a>)
+    Operator(OperatorExecutable)
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Num(operand) => write!(f, "{}", operand),
+            Self::Operator(executable) => write!(f, "{}", executable)
+        }
+    }
 }
