718708e084669802660bbb120718f52295d4b7a4
Jayden <179274165+JaydenHuang555@users.noreply.github.com>
2025-12-29 15:54:18 -0800

    add operator

--- src/token/operator/executables.rs
+++ src/token/operator/executables.rs
@@ -1,3 +1,4 @@
+
 use crate::token::operator::executable::{OperatorExecutable, prec};
 
 pub const ADD: OperatorExecutable =
@@ -9,6 +10,19 @@ pub const MULTI: OperatorExecutable =
     OperatorExecutable::new('*', prec('*'), |left, right| -> f64 { left * right });
 pub const DIV: OperatorExecutable =
     OperatorExecutable::new('/', prec('/'), |left, right| -> f64 { left / right });
+
+pub const EXPONENT: OperatorExecutable = OperatorExecutable::new('^', prec('^'), |base, exponent| -> f64 {
+    match exponent {
+        0.0 => 1.0,
+        1.0 => base,
+        _ => {
+            match exponent.fract() {
+                0.0 => base.powi(exponent as i32),
+                _ => base.powf(exponent)
+            }
+        }
+    }
+});
 
 pub const fn match_char_with_executable(value: char) -> Option<OperatorExecutable> {
     match value {
